fe1988ab68acefe89dbdeb5acd1aaf7aa2ad3f2d
Logan Raschke <56165605+rasc1778@users.noreply.github.com>
2019-11-26 11:28:23 -0800

    follow camera

--- Assets/Challenge 1/Scripts/FollowPlane.cs
+++ Assets/Challenge 1/Scripts/FollowPlane.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class FollowPlane : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject plane;
     private Vector3 offset = new Vector3(30, 0, 10);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = plane.transform.position + offset;
     }
 }
